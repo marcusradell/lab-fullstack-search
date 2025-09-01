95459fdbc3dc5b391cc6c32e7e1016eb8d5ed76b
Marcus Rådell <marcus@radell.net>
2025-09-01 09:05:06 +0200

    remove features barrel

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Search } from "./features";
+import { Search } from "./features/search";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
