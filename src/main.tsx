707de7cbe9df6b6d0c057b4bc376160315889bd0
Marcus Rådell <marcus@radell.net>
2025-09-01 08:59:36 +0200

    refactor: remove search feature components and related files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Search } from "./Search";
+import { Search } from "./features";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/search-components.tsx
+++
@@ -1,6 +0,0 @@
-// This file is kept for backward compatibility
-// Components have been moved to individual files in the components directory
-export { SearchInput } from "./components/search-input";
-export { SearchButton } from "./components/search-button";
-export { SearchControls } from "./components/search-controls";
-export { ErrorMessage } from "./components/error-message";
