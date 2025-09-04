73577113adf7874d6292cd054a74346cce86d9c9
Marcus Rådell <marcus@radell.net>
2025-09-04 14:37:02 +0200

    remove nav items

--- src/components/top-nav/nav-items.tsx
+++
@@ -1,7 +0,0 @@
-type NavItemsProps = {
-  children: React.ReactNode;
-};
-
-export function NavItems({ children }: NavItemsProps) {
-  return <div className="flex gap-6">{children}</div>;
-}
--- src/components/top-nav/top-nav.tsx
+++ src/components/top-nav/top-nav.tsx
@@ -1,17 +1,12 @@
 import { NavContainer } from "./nav-container";
 import { NavContent } from "./nav-content";
 import { NavLogo } from "./nav-logo";
-import { NavItems } from "./nav-items";
-import { NavItem } from "./nav-item";
 
 export function TopNav() {
   return (
     <NavContainer>
       <NavContent>
         <NavLogo text="SEARCH" />
-        <NavItems>
-          <NavItem text="Home" isActive={true} />
-        </NavItems>
       </NavContent>
     </NavContainer>
   );
